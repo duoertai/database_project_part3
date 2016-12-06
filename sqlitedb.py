33c62a0951b4887d47e2049801c5d5c4023cef14
duoertai <duoertai@gmail.com>
2016-12-05 22:54:37 -0800

    mend

--- sqlitedb.py
+++ sqlitedb.py
@@ -30,13 +30,10 @@ def transaction():
 
 # returns the current time from your database
 def getTime():
-    # TODO: update the query string to match
-    # the correct column and table name in your database
-    query_string = 'select currenttime from Time'
+    query_string = 'select Time from CurrentTime'
     results = query(query_string)
     # alternatively: return results[0]['currenttime']
-    return results[0].currenttime # TODO: update this as well to match the
-                                  # column name
+    return results[0].Time
 
 # returns a single item specified by the Item's ID in the database
 # Note: if the `result' list is empty (i.e. there are no items for a
